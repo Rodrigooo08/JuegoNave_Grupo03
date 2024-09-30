37aeda46945e19cde964ec5ca977ef789be45cb3
Rodrigo Fines <112434197+Rodrigooo08@users.noreply.github.com>
2024-09-30 17:34:42 -0300

    Actualizacion de escena3

--- src/scenes/Escena2.js
+++ src/scenes/Escena2.js
@@ -67,9 +67,9 @@ class Escena2 extends Phaser.Scene{
         this.puntaje +=1;
         this.textoPuntaje.setText('Puntaje: '+this.puntaje);
         //condicion para pasar de escena
-        /*if(this.puntaje >= 1500){
-            this.scene.start('Escene3',{puntaje:this.puntaje,balasReceolectadas: this.balasRecolectadas});
-        }*/
+        if(this.puntaje >= 1500){
+            this.scene.start('Escena3',{puntaje:this.puntaje,balasReceolectadas: this.balasRecolectadas});
+        }
     }
 
     gameOver(jugador,meteoro){
