0e9aca03afc4a9ca8dbfea0d6cd0e9fed0d0e5fa
Rodrigo Fines <112434197+Rodrigooo08@users.noreply.github.com>
2024-09-30 21:47:24 -0300

    Agrego archivo css, con fondo animado y modificacion en los codigos

--- src/index.js
+++ src/index.js
@@ -3,10 +3,22 @@ import Escena2 from "./scenes/Escena2.js"
 import GameOver from "./scenes/GameOver.js";
 import MenuStart from "./scenes/MenuStart.js"; 
 import Escena3 from "./scenes/Escena3.js";
+// Función para cargar el CSS
+function loadCSS(file) {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = file;
+    document.head.appendChild(link);
+}
+// Cargar el archivo CSS
+loadCSS('styles.css');
+
 const config={
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    parent:'game-container',
     scene: [MenuStart,Escena1, Escena2, Escena3,GameOver],
     physics:{
         default: 'arcade',
