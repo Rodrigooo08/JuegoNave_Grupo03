910a4b2e01534ba81729fc268353a0240a826566
CintiaFlores <108830824+CintiaFlores@users.noreply.github.com>
2024-10-09 20:50:46 -0300

    Revert "Agregado de MenuStart"
    This reverts commit 919b93c59640dc72848e0a18c9d64eabe352bfdb.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import Escena1 from "./scenes/Escena1.js"
 import Escena2 from "./scenes/Escena2.js"
-import GameOver from "./scenes/GameOver.js"
-import MenuStart from "./scenes/MenuStart.js";   
-
+import GameOver from "./scenes/GameOver.js";
 
 const config={
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    scene: [MenuStart, Escena1, Escena2,GameOver],
+    scene: [Escena1, Escena2,GameOver],
     physics:{
         default: 'arcade',
         arcade:{
